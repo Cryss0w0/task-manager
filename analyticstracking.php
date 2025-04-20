3ee3a987b005e12d4c6240006eccb4ed6f093835
Cryss0w0 <punpunpunaltha@gmail.com>
2025-04-20 18:27:07 +0800

    Update analyticstracking.php

--- analyticstracking.php
+++ analyticstracking.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HKXWPDFC3F"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-055FLHHMF9"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-HKXWPDFC3F');
+  gtag('config', 'G-055FLHHMF9');
 </script>
